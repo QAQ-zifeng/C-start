7730fbd9c3e0fd840ecad335f201e45eb599b719
zhu-zifeng <1951725560@qq.com>
2022-08-17 15:30:30 +0800

    OK

--- test_6_1/test.c
+++ test_6_1/test.c
@@ -114,8 +114,69 @@ void test()
 	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
 }
 
+
+int cmp_int(const void* e1, const void* e2)
+{
+	return(*(int*)e1 - *(int*)e2);//升序
+	//return(*(int*)e2 - *(int*)e1);//降序
+}
+
+void Swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+void bubble_sort(void* base, int sz, int width, int(*cmp)(const void* e1, const void* e2))
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+void test1()
+{
+	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+	//0 1 2 3 4 5 6 7 8 9
+	//把数组排成升序
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//bubble_sort(arr, sz);
+
+	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+}
+
+
+
 int main()
 {
-	test();
+	//test();
+	test1();
 	return 0;
 }
