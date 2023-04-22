e094fe48b54daefe117f6c9b7acf83a0f113440d
zhu-zifeng <1951725560@qq.com>
2023-04-22 20:36:44 +0800

    OK

--- mine_clearing/game.h
+++ mine_clearing/game.h
@@ -10,7 +10,9 @@
 #define COLS COL+2
 #define EASY_RET 10
 
-void InitBoard(char board[ROWS][COLS], int rows, int cols, char tmp);
-void DisPlay(char board[ROWS][COLS], int row, int col);
-void SetMine(char board[ROWS][COLS], int row, int col);
-ClearMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
+
+//函数的声明
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char tmp);//初始化每一个方块
+void DisPlay(char board[ROWS][COLS], int row, int col);//显示界面
+void SetMine(char board[ROWS][COLS], int row, int col);//设置雷的个数
+ClearMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);//扫雷过程
